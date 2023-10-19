ebd0b9c87917ffc48a0948a922cd0623b69adbd6
Anton Kuptsov <39290991+anton-kuptsov@users.noreply.github.com>
2023-10-19 18:42:08 +0300

    fix: add import NUM_OF_LETTERS_ALLOWED

--- src/components/GuessInput.jsx
+++ src/components/GuessInput.jsx
@@ -1,3 +1,5 @@
+import { NUM_OF_LETTERS_ALLOWED } from "../constants";
+
 export function GuessInput({ onSubmit, onChange, value, disabled }) {
   return (
     <form className="guess-input-wrapper" onSubmit={onSubmit}>
